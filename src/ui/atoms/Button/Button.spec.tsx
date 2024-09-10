dd7c30ea5b16e637d081f2b7a1634994f10aa5bd
Patryk Omiotek <patryk.omiotek@gmail.com>
2024-09-10 12:25:58 +0200

    feat: with tests

---
+++ src/ui/atoms/Button/Button.spec.tsx
@@ -0,0 +1,20 @@
+import { render } from '@testing-library/react';
+import { Button } from './Button';
+import { axe } from 'jest-axe';
+
+// vitest + react testing library
+
+describe('Button component', () => {
+  it('should render correctly', () => {
+    render(<Button>Click me</Button>);
+    // make an assertion
+    // provide callback and check number of calls of this callback
+  });
+  it('it should not have violations', async () => {
+    const { container, debug } = render(<Button>Click me</Button>);
+    const result = await axe(container);
+    // console.log(container);
+    // debug();
+    expect(result).toHaveNoViolations();
+  });
+});
---
+++ vitest-setup.ts
@@ -0,0 +1,22 @@
+import { afterEach, afterAll, beforeEach } from 'vitest';
+// import { cleanup } from '@testing-library/react'
+import '@testing-library/jest-dom/vitest';
+import '@testing-library/jest-dom';
+
+import { toHaveNoViolations } from 'jest-axe';
+// import { server } from "./src/mocks/node";
+
+expect.extend(toHaveNoViolations);
+
+beforeEach(() => {
+  // server.listen();
+});
+
+afterEach(() => {
+  // cleanup();
+  // server.resetHandlers();
+});
+
+afterAll(() => {
+  // server.close();
+});
