b29ca383a20668296d035b6171722255e8f0e86c
Patryk Omiotek <patryk.omiotek@gmail.com>
2024-09-10 11:21:52 +0200

    feat: storybook intro

---
+++ src/ui/atoms/Button/Button.stories.ts
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { fn } from '@storybook/test';
+import { Button } from './Button';
+
+const meta = {
+  title: 'UI/Atoms/Button',
+  component: Button,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  args: { onClick: fn() },
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const Primary: Story = {
+  args: {
+    children: 'Click me',
+  },
+};
