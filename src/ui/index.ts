52f7ba6d5f96030309807b874cf84a6177dd3a85
Patryk Omiotek <patryk.omiotek@gmail.com>
2024-09-10 08:19:08 +0200

    feat: atoms and molecules

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -3,6 +3,6 @@
 
 // export { Button };
 
-export { Button } from './Button';
-export { Text } from './Text';
-export { Input } from './Input';
+export { Button } from './atoms/Button';
+export { Text } from './atoms/Text';
+export { Input } from './molecules/Input';
